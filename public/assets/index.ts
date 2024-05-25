fc64f14dd09c7d778e653b4e4a1d603387b5b524
Anmeet Sekhon <113476064+AnmeetS@users.noreply.github.com>
2024-05-24 22:04:25 -0400

    Coming Soon Page

---
+++ public/assets/index.ts
@@ -0,0 +1,4 @@
+import companyLogo from './images/companyLogo/cursedCarbonLogo.jpg';
+import transparentCompanyLogo from './images/companyLogo/CuredCarbon-04.png';
+
+export{companyLogo, transparentCompanyLogo};
